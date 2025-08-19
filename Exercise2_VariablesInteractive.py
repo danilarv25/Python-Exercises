c339c2629d2e7178e1d5f785762a731ea1ce2359
Daniel Larva <daniel.larva8@gmail.com>
2025-08-19 11:38:46 +0300

    Started interactive file

---
+++ Exercise2_VariablesInteractive.py
@@ -0,0 +1,8 @@
+name = input("What is your name?\n")
+
+print("Nice to meet you, "+name+"!")
+
+rad = int(input("Now please give the radius of a circle: "))
+area = 3.14*rad*rad
+area = str(area)
+print("If the area is "+rad+", the area is: "+area)
