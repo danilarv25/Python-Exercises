462c2f1cc847d9c00c4a903b7dd556a7a8dc3e31
Daniel Larva <daniel.larva8@gmail.com>
2025-08-20 21:14:51 +0300

    Part 3 (rectangle) and part 4 (sum, product, avg)

--- Exercise2_VariablesInteractive.py
+++ Exercise2_VariablesInteractive.py
@@ -14,5 +14,30 @@ length = float(input("Please give the length and width of a rectangle, starting 
 width = float(input("And now the width, please: "))
 area = length*width
 perim = (length*2) + (width*2)
-print("With a length of "+str(length)+" units and a width of "+str(width)+" units, the area is: "+str(area))
-print("The perimeter is then: "+str(perim))
+print("With a length of "+str(length)+" units and a width of "+str(width)+" units, the area is: "+str(area)+" square units")
+print("The perimeter is then: "+str(perim)+" units")
+
+'''Part 4'''
+numlist = []
+turns = 0
+while turns < 3:
+    num = int(input("Please give a number: "))
+    numlist.append(num)
+    turns += 1
+
+sum = 0
+for i in numlist:
+    sum += i
+print("The sum of "+str(numlist)+" is: "+str(sum))
+
+prod = numlist[0]
+prod = int(prod)
+for i in numlist:
+    if i == numlist[0]:
+        pass
+    else:
+        prod = prod*i
+print("While the product is: "+str(prod))
+
+avrg = prod / turns
+print("And the average of the numbers is: "+str(avrg))
