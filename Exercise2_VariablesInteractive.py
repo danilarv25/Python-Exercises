305d69f5647398bbfd0bbedd12561005fc378ab8
Daniel Larva <daniel.larva8@gmail.com>
2025-08-20 20:37:47 +0300

    Completed part 2 of exercise (area of circle) and started part 3 (rectangle)

--- Exercise2_VariablesInteractive.py
+++ Exercise2_VariablesInteractive.py
@@ -1,8 +1,18 @@
+'''Part 1'''
 name = input("What is your name?\n")
 
 print("Nice to meet you, "+name+"!")
 
-rad = int(input("Now please give the radius of a circle: "))
+'''Part 2'''
+rad = float(input("Now please give the radius of a circle: "))
 area = 3.14*rad*rad
 area = str(area)
-print("If the area is "+rad+", the area is: "+area)
+print("If the area is "+str(rad)+", the area is: "+area)
+
+'''Part 3'''
+length = float(input("Please give the length and width of a rectangle, starting with the length: "))
+width = float(input("And now the width, please: "))
+area = length*width
+perim = (length*2) + (width*2)
+print("With a length of "+str(length)+" units and a width of "+str(width)+" units, the area is: "+str(area))
+print("The perimeter is then: "+str(perim))
