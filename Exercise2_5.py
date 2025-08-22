ad25072095e2ec6f43c53acdc9ef85d31c73d0ad
Daniel Larva <daniel.larva8@gmail.com>
2025-08-22 15:25:01 +0300

    cleaned exercise2part1 a bit, 22-08 in-class assignment

--- Exercise2_5.py
+++ Exercise2_5.py
@@ -5,8 +5,7 @@ talents = talents*20*32*13.3
 pounds = pounds*32*13.3
 lots = lots*13.3
 weight = talents+pounds+lots
-kg = weight//1000
-kg = int(kg)
+kg = int(weight//1000)
 grams = weight-(kg*1000)
 grams = round(grams, 2)
 print("The weight in modern units:\n"+str(kg)+" kilograms and "+str(grams)+" grams.")
