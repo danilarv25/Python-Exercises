e1d81e58f8c38e9f54c844c6b115ba7f650e2bb0
Daniel Larva <daniel.larva8@gmail.com>
2025-11-03 12:13:07 +0200

    module11 task 2 "cars"

---
+++ Module11/module11_2_cars.py
@@ -0,0 +1,104 @@
+import random
+import pandas as pd
+
+class Car:
+    def __init__(self, registration, max_speed,
+                 current_speed = 0, travelled_distance = 0):
+
+        self.registration = registration
+        self.max_speed = max_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+
+    def accelerate(self, kmh:int):
+        if (self.current_speed + kmh) <= 0:
+            self.current_speed = 0
+        elif self.current_speed <= (self.max_speed - kmh):
+            self.current_speed += kmh
+        elif self.current_speed > (self.max_speed - kmh):
+            self.current_speed = self.max_speed
+
+    def drive(self, hours:float):
+        self.travelled_distance += abs(self.current_speed)*hours
+
+class ElectricCar(Car):
+    def __init__(self, registration, max_speed, capacity):
+        super().__init__(registration, max_speed)
+        self.capacity = capacity
+
+    def accelerate(self, kmh:int):
+        super().accelerate(kmh)
+
+    def drive(self, hours:float):
+        super().drive(hours)
+
+class GasolineCar(Car):
+    def __init__(self, registration, max_speed, capacity):
+        super().__init__(registration, max_speed)
+        self.capacity = capacity
+
+    def accelerate(self, kmh:int):
+        super().accelerate(kmh)
+
+    def drive(self, hours:float):
+        super().drive(hours)
+
+class Race:
+    def __init__(self, name:str, km:int, racer_list):
+        self.name = name
+        self.km = km
+        self.racer_list = racer_list
+
+    def hour_passes(self):
+        for i in self.racer_list:
+            i.accelerate(random.randint(-10, 15))
+            i.drive(1)
+
+    def print_status(self):
+        info = []
+        for i in self.racer_list:
+            racer = []
+            racer.append(i.registration)
+            racer.append(i.max_speed)
+            racer.append(i.current_speed)
+            racer.append(i.travelled_distance)
+            info.append(racer)
+        print(f"\n{self.name} stats:")
+        df = pd.DataFrame(info, columns=['Registration', 'Max Speed', 'Current Speed', 'Travelled Distance'])
+        print(df)
+
+    def race_finished(self):
+        for i in self.racer_list:
+            if i.travelled_distance >= self.km:
+                return True
+
+
+ecar = ElectricCar("ABC-15", 180, 52.5)
+gcar = GasolineCar("ABC-123", 165, 32.3)
+
+# cars = []
+# num = 1
+#
+# for i in range(10):
+#     reg = "ABC-"+str(num)
+#     max = random.randint(100,200)
+#     cars.append(Car(reg, max))
+#     num+=1
+#
+# print("Our racers:")
+# for i in cars:
+#     print(f"{i.registration} with a max speed of {i.max_speed} km/hr")
+#
+# race = Race("Grand Demolition Derby", 8000, cars)
+#
+# hour = 1
+# # not_racing = False
+# print("\nRacing...\n")
+# while not race.race_finished():
+#     race.hour_passes()
+#     hour += 1
+#     # not_racing = race.race_finished()
+#     if hour % 10 == 0:
+#         race.print_status()
+# print(f"\nHours passed: {hour}")
+# race.print_status()
